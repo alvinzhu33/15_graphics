4a5e289aede8eae9fa41d965dcce48cdcb87a27b
Alvin Zhu <alvinzhu33@yahoo.com>
2017-03-16 15:30:05 -0400

    Quick fix

--- matrix.py
+++ matrix.py
@@ -13,7 +13,7 @@ def make_hermite():
 
 def make_bezier():
     t = [[-1, 3, -3, 1], [3, -6, -3, 0], [-3, 3, 0, 0], [1, 0, 0, 0]];
-    print_matrix(transpose(t));
+    return transpose(t);
 
 def generate_curve_coefs( p1, p2, p3, p4, t ):
     pass
